e708910a5d8319783a2d1a2b834e28169feadbf1
Bhavya337 <72799302+Bhavya337@users.noreply.github.com>
2020-12-22 16:10:48 +0530

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -1,7 +1,8 @@
-function add_user(){
-    player_1=document.getElementById("Player1_name").value;
-    player_2=document.getElementById("Player2_name").value;
-    localStorage.setItem("player1",player_1);
-    localStorage.setItem("player2",player_2);
- window.location="game_page.html";
- } 
+function add(){
+    var player1_name=document.getElementById("player1_name_input").value;
+    var player2_name=document.getElementById("player2_name_input").value;
+    localStorage.setItem("player1 name",player1_name);
+    localStorage.setItem("player2 name",player2_name);
+    window.location="game_page.html";
+    
+}
